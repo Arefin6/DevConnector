99aac15d78e3fbce0d2032f40c2edb13ff21244a
Arefi6 <arefinhossain3@gmail.com>
2020-12-31 13:46:01 +0600

    basic set up done

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+ import express from 'express';
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('Hello Arefin');
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,console.log(`Listening on port ${port}`))
