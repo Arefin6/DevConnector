825b8a78639d52f5617cad072627a2595f2dcaa6
Arefi6 <arefinhossain3@gmail.com>
2021-01-01 15:49:44 +0600

    User login with token is done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
-import gravatar from 'gravatar'
+import gravatar from 'gravatar';
+import generateAuthToken from '../utilities/generateAuthToekn.js' 
 
 //@route Get /api/User/Test
 //@desc Tet route
@@ -37,9 +38,56 @@ const registerUser = asyncHandler(async (req,res) =>{
         password,
         avatar
     })
-    
-    res.send(user)
+      if(user){
+          res.status(201).json({
+            _id:user.id,
+            name:user.name,
+            email:user.email,
+            avatar,
+            token:generateAuthToken(user._id)
+          })
+      }
+      else{
+          res.status(404)
+          res.json({
+          message:"Invalid user data" 
+        })
+      }    
+  
  }
 })
 
-export {testApi,registerUser}
+
+//@route Post /api/user/login
+//@desc Login User
+//@access public
+
+const authUser = asyncHandler(async (req,res) =>{ 
+   
+    const{email,password} = req.body
+    
+    const user = await User.findOne({email})
+    
+    if(user && (await user.matchPassword(password))){
+       
+        res.send({
+            _id:user.id,
+            name:user.name,
+            email:user.email,
+            avatar:user.avatar,
+            token:generateAuthToken(user._id)
+        })
+    }
+    else{
+        
+        res.status(401)
+          res.json({
+          message:"Invalid user data" 
+        })
+    }
+
+})
+
+
+
+export {testApi,registerUser,authUser}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { registerUser, testApi } from '../controllers/userController.js';
+import { authUser, registerUser, testApi } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/test',testApi)
 router.post('/register',registerUser)
+router.post('/login',authUser)
 
 export default router
