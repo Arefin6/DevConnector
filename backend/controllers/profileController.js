0f87a39dda62fea41b636f032ca2c723f7cc1522
Arefi6 <arefinhossain3@gmail.com>
2021-01-02 13:07:25 +0600

    Basic Profile creation done

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,5 +1,6 @@
 import Profile from '../models/ProfileModel.js'
 import asyncHandler from 'express-async-handler'
+import normalize from 'normalize-url'
 
 //@route Get /api/profile/test
 //@desc Tet route
@@ -29,4 +30,59 @@ const getCurrentProfile = asyncHandler(async(req,res) =>{
 
 })
 
-export {testRoute,getCurrentProfile}
+
+//@route POST /api/profile
+//@desc Get A Profile
+//@access private
+
+const createProfile = asyncHandler(async(req,res) =>{
+    const {
+        website,
+        skills,
+        status,
+        youtube,
+        twitter,
+        instagram,
+        linkedin,
+        facebook,
+        bio,
+        // spread the rest of the fields we don't need to check
+        ...rest
+      } = req.body;
+
+      const socialFields = { youtube, twitter, instagram, linkedin, facebook };
+
+    // normalize social fields to ensure valid url
+    for (const [key, value] of Object.entries(socialFields)) {
+      if (value && value.length > 0)
+        socialFields[key] = normalize(value, { forceHttps: true });
+    }
+
+    const createdProfile = await Profile.create({
+        user:req.user._id,
+        website:
+        website && website !== ''
+          ? normalize(website, { forceHttps: true })
+          : '',
+          skills: Array.isArray(skills)
+          ? skills
+          : skills.split(',').map((skill) => ' ' + skill.trim()),
+          status,  
+          social:socialFields,
+          bio,
+       ...rest
+    });
+
+    if(createdProfile){
+        res.status(201).json(createdProfile)
+    }
+    else{
+        res.status(500)
+        res.send({message:"something went wrong"})
+    }
+
+   
+ })
+
+
+export {testRoute,getCurrentProfile,createProfile}
--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { getCurrentProfile, testRoute } from '../controllers/profileController.js';
+import { createProfile, getCurrentProfile, testRoute } from '../controllers/profileController.js';
 import  {protect} from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/test',testRoute)
-router.route('/').get(protect,getCurrentProfile)
+router.route('/')
+.get(protect,getCurrentProfile)
+.post(protect,createProfile)
 
 export default router
