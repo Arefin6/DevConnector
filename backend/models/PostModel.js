cfee11c1f0632afa3e3240024def7cec2bffd8d4
Arefi6 <arefinhossain3@gmail.com>
2021-01-03 14:52:48 +0600

    Post Model Added

---
+++ backend/models/PostModel.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose'
+
+const postSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+      },
+    text: {
+        type:String,
+        required:true
+    },
+
+    name:{
+        type:String
+    },
+    avatar:{
+        type:String
+    },
+    likes:[
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'users'
+              }   
+        }
+    ],
+    comments:[
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'users'
+              },
+             text:{ 
+              type:String,
+              required:true
+          },
+      
+          name:{
+              type:String
+          },
+          avatar:{
+              type:String
+          },
+          date:{
+             type:Date,
+             default:Date.now()
+          }  
+        }
+    ]
+ },
+ {
+     timestamps:true,
+ },
+ );
+
+ const Post = mongoose.model('Post',postSchema)
+
+ export default Post
