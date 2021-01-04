20a3ba623861dd39f4c75495ea5422b4ecef9967
Arefi6 <arefinhossain3@gmail.com>
2021-01-04 22:17:11 +0600

    Login Screen Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
+import LoginScreen from './screens/LoginScreen/LoginScreen';
 
 function App() {
   return (
@@ -18,6 +19,12 @@ function App() {
          <Switch>
            <Route path="/" exact>
             <HomeScreen></HomeScreen>
+           </Route>
+           <Route path="/login" exact>
+            <LoginScreen></LoginScreen>
+           </Route>
+           <Route path="*" exact>
+            <h2>Not Found</h2>
            </Route>
          </Switch>
        
